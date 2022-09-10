2a400e269d3df3e37b6c919ca66b54fe4a51372b
SATO-Hiroki-tbn <sato1208hiro@gmail.com>
2022-09-10 12:52:09 +0900

    Userを登録できるようにする

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,14 +1,14 @@
 module Mutations
   class CreateUser < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    field :user, Types::UserType, null: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :name, String, required: true
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(params)
+      user = User.create(name: params[:name])
+      {
+        "user": user
+      }
+    end
   end
 end
